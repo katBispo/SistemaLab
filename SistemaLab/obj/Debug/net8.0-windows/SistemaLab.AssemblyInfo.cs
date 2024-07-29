0f1eba736cf4c6b0ed393c08fa22cae0552ccab4
Kauan <kauankenney16752@gmail.com>
2024-07-29 20:06:15 -0300

    ReagenteController

--- SistemaLab/obj/Debug/net8.0-windows/SistemaLab.AssemblyInfo.cs
+++ SistemaLab/obj/Debug/net8.0-windows/SistemaLab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d74a69dfda718b193432776e89b20f977debff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd7b3b5ab35d4269bbf3518ff7878fda012bf0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
